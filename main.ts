76776bc58978c6bd8d147a5f5e6f52add5dd1136
WMs784 <64195816+WMs784@users.noreply.github.com>
2021-06-19 18:25:06 +0900

    更新 pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,39 @@
 let num = 0;
-let list = [ArrowNames.North,ArrowNames.South,ArrowNames.East,ArrowNames.West,
-ArrowNames.NorthEast,ArrowNames.NorthWest,ArrowNames.SouthEast,ArrowNames.SouthWest];
-    led.plot(num,4);
-    radio.setGroup(1);
+let list = [ArrowNames.North,ArrowNames.NorthEast,ArrowNames.East,ArrowNames.SouthEast,
+ArrowNames.South,ArrowNames.SouthWest,ArrowNames.West,ArrowNames.NorthWest];
+let display :grove.TM1637 = null;
+display = grove.createDisplay(DigitalPin.P0, DigitalPin.P14);
+led.plot(num,4);
+radio.setGroup(1);
+let direction = input.compassHeading();
 input.onButtonPressed(Button.A, function () {
-        radio.sendNumber(num);
-        //led.unplot(num,4);
-        //basic.clearScreen();
-        basic.showNumber(num);
-    })
-    input.onButtonPressed(Button.B, function () {
-        basic.clearScreen();
-        //led.unplot(num,4);
-        led.plot(num, 4);
-        num++;
-        num %= 5;
-    })
-    radio.onReceivedNumber(function (recievednumber: number) {
-        //basic.clearScreen();
-        //led.unplot(num,4);
-        basic.showArrow(list[recievednumber]);
-        console.log(recievednumber);
-        //console.log(num);
-    })
+    radio.sendString("i"+num.toString());
+    basic.pause(100);
+    radio.sendString("d"+direction.toString());
+    //basic.showNumber(num);
+    display.show(num+1);
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen();
+    num++;
+    num %= 5;
+    led.plot(num, 4);
+})
+let cd = 0;
+radio.onReceivedString(function (rs: string) {
+    //basic.clearScreen();
+    //basic.showString(rs[0]);
+    let n = parseInt(rs.substr(1));
+    if(rs[0] == 'd'){//目的地からの角度を受け取ったとき
+        cd = n;
+    }
+    else{//方角番号を受け取った時
+        basic.showArrow(list[n]);
+    }
+})
+if((input.compassHeading() - cd)**2 <400 && cd != 0){
+    basic.showArrow(ArrowNames.North);
+}
+else{
+    basic.showIcon(IconNames.Angry);
+}
