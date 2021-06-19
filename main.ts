66999920f021f5dcf55993fc23a9f7b46f3e1211
WMs784 <64195816+WMs784@users.noreply.github.com>
2021-06-19 14:26:27 +0900

    更新 pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,25 @@
 let num = 0;
-let list = [ArrowNames.North,ArrowNames.South,ArrowNames.East,ArrowNames.West,ArrowNames.NorthEast];
-basic.forever(function () {
-    led.plot(num, 4);
+let list = [ArrowNames.North,ArrowNames.South,ArrowNames.East,ArrowNames.West,
+ArrowNames.NorthEast,ArrowNames.NorthWest,ArrowNames.SouthEast,ArrowNames.SouthWest];
+    led.plot(num,4);
     radio.setGroup(1);
-    input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function () {
         radio.sendNumber(num);
+        //led.unplot(num,4);
+        //basic.clearScreen();
+        basic.showNumber(num);
     })
     input.onButtonPressed(Button.B, function () {
-        led.unplot(num,4);
+        basic.clearScreen();
+        //led.unplot(num,4);
+        led.plot(num, 4);
         num++;
         num %= 5;
     })
     radio.onReceivedNumber(function (recievednumber: number) {
-        led.unplot(num,4);
+        //basic.clearScreen();
+        //led.unplot(num,4);
         basic.showArrow(list[recievednumber]);
         console.log(recievednumber);
         //console.log(num);
     })
-})
