8212413d0972b0d1dc91ea47ad6ddd498ce9462a
WMs784 <64195816+WMs784@users.noreply.github.com>
2021-06-20 15:23:46 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ let num = 0;
 let list = [ArrowNames.North,ArrowNames.NorthEast,ArrowNames.East,ArrowNames.SouthEast,
 ArrowNames.South,ArrowNames.SouthWest,ArrowNames.West,ArrowNames.NorthWest];
 let display :grove.TM1637 = null;
-display = grove.createDisplay(DigitalPin.P0, DigitalPin.P14);
+display = grove.createDisplay(DigitalPin.P2, DigitalPin.P16);
 led.plot(num,4);
 radio.setGroup(1);
 let direction = input.compassHeading();
@@ -10,13 +10,18 @@ input.onButtonPressed(Button.A, function () {
     radio.sendString("i"+num.toString());//目的地番号を送る
     basic.pause(100);
     radio.sendString("d"+direction.toString());//現在の方角を送る
-    basic.showNumber(num+1);
+    display.show(num+1);
 })
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen();//一回全部消す
     num++;//一つ右に移動
     num %= 5;//5は範囲外なので0に戻す
     led.plot(num, 4);//新しい位置で再点灯
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen();
+    cd = -1;
+    led.plot(num,4);
 })
 let cd = -1;
 radio.onReceivedString(function (rs: string) {
