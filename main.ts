9cd201ee356f31a0653d16383b43770b2a6a9f3d
WMs784 <64195816+WMs784@users.noreply.github.com>
2021-06-20 13:03:16 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ input.onButtonPressed(Button.A, function () {
     radio.sendString("i"+num.toString());//目的地番号を送る
     basic.pause(100);
     radio.sendString("d"+direction.toString());//現在の方角を送る
-    display.show(num+1);
+    basic.showNumber(num+1);
 })
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen();//一回全部消す
@@ -23,14 +23,20 @@ radio.onReceivedString(function (rs: string) {
     let n = parseInt(rs.substr(1));
     if(rs[0] == 'd'){//目的地からの角度を受け取ったとき
         cd = n;
+        display.show(cd);
     }
     else{//方角番号を受け取った時
         basic.showArrow(list[n]);//listから方角を探して矢印を出力
+        //display.show(n);
     }
 })
-if((input.compassHeading() - cd)**2 <400 && cd>=0){//正しい方向に向いた時
-    basic.showArrow(ArrowNames.North);//真っ直ぐの矢印を出力
-}
-else if(cd >= 0){//正しい方向じゃない時
-    basic.showIcon(IconNames.Angry);//怒る
-}
+basic.forever(function () {
+    if((input.compassHeading() - cd)**2 <900 && cd>=0){//正しい方向に向いた時
+        basic.showArrow(ArrowNames.North);//真っ直ぐの矢印を出力
+        basic.clearScreen();
+    }
+    else if(cd >= 0){//正しい方向じゃない時
+        basic.showIcon(IconNames.Angry);//怒る
+        soundExpression.giggle.play();
+    }
+})
