216e95d99d5d02c507b60de0bead7786a1022c25
WMs784 <64195816+WMs784@users.noreply.github.com>
2021-06-20 10:34:43 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -7,33 +7,30 @@ led.plot(num,4);
 radio.setGroup(1);
 let direction = input.compassHeading();
 input.onButtonPressed(Button.A, function () {
-    radio.sendString("i"+num.toString());
+    radio.sendString("i"+num.toString());//目的地番号を送る
     basic.pause(100);
-    radio.sendString("d"+direction.toString());
-    //basic.showNumber(num);
+    radio.sendString("d"+direction.toString());//現在の方角を送る
     display.show(num+1);
 })
 input.onButtonPressed(Button.B, function () {
-    basic.clearScreen();
-    num++;
-    num %= 5;
-    led.plot(num, 4);
+    basic.clearScreen();//一回全部消す
+    num++;//一つ右に移動
+    num %= 5;//5は範囲外なので0に戻す
+    led.plot(num, 4);//新しい位置で再点灯
 })
-let cd = 0;
+let cd = -1;
 radio.onReceivedString(function (rs: string) {
-    //basic.clearScreen();
-    //basic.showString(rs[0]);
     let n = parseInt(rs.substr(1));
     if(rs[0] == 'd'){//目的地からの角度を受け取ったとき
         cd = n;
     }
     else{//方角番号を受け取った時
-        basic.showArrow(list[n]);
+        basic.showArrow(list[n]);//listから方角を探して矢印を出力
     }
 })
-if((input.compassHeading() - cd)**2 <400 && cd != 0){
-    basic.showArrow(ArrowNames.North);
+if((input.compassHeading() - cd)**2 <400 && cd>=0){//正しい方向に向いた時
+    basic.showArrow(ArrowNames.North);//真っ直ぐの矢印を出力
 }
-else{
-    basic.showIcon(IconNames.Angry);
+else if(cd >= 0){//正しい方向じゃない時
+    basic.showIcon(IconNames.Angry);//怒る
 }
